ccdca14fe018e055f5ccf4ebe0f9d3d53be54efe
Tony Tan <doublskyp@gmail.com>
2015-09-14 14:11:38 -0500

    Update edge_draw.h

--- edge_draw.h
+++ edge_draw.h
@@ -1,6 +1,11 @@
 #ifndef MY_EDGEDRAW_H
 #define MY_EDGEDRAW_H
 #include <stdio.h>
+
+#define X_SIZE 76
+#define Y_SIZE 95
+
 void edge_draw(unsigned char *in,unsigned char *mid,int x_size,int y_size);
+
 #endif
 
