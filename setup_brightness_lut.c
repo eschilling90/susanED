0b4077c6a7f5e1709383fbcc2eda29b7e4a81d6c
Erik <erik.schilling@gmail.com>
2015-09-13 22:02:13 -0500

    added setup_brightness changes for bp conversion

--- setup_brightness_lut.c
+++ setup_brightness_lut.c
@@ -1,17 +1,12 @@
 #include "setup_brightness_lut.h"
 
 void setup_brightness_lut(bp)
-  unsigned char **bp;
+  unsigned char bp[516];
 {
 int   k;
 float temp;
-int tmp_ary[516];
 int thresh=20;
 int form=6;
-
-//  *bp=(unsigned char *)malloc(516);
-  *bp=(unsigned char *) tmp_ary; //added by AJG
-  *bp=*bp+258;
 
   for(k=-256;k<257;k++)
   {
@@ -20,7 +15,7 @@ int form=6;
     if (form==6)
       temp=temp*temp*temp;
     temp=100.0*exp(-temp);
-    *(*bp+k)= (unsigned char)temp;
+    bp[k+258]= (unsigned char)temp;
   }
   
 }
--- setup_brightness_lut.h
+++ setup_brightness_lut.h
@@ -2,6 +2,6 @@
 #define MY_SETUPBRIGHTNESS_H
 #include <stdio.h>
 #include <math.h>
-void setup_brightness_lut(unsigned char **bp);
+void setup_brightness_lut(unsigned char bp[516]);
 #endif
 
