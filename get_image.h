fd4f9724baa7371bb51dd75d31dafc7a6d6c5f0b
Tony Tan <doublskyp@gmail.com>
2015-09-15 13:43:53 -0500

    convert *in to array

--- get_image.h
+++ get_image.h
@@ -1,6 +1,10 @@
 #ifndef MY_GETIMAGE_H
 #define MY_GETIMAGE_H
 #include <stdio.h>
-void get_image(char filename[200],unsigned char **in,int x_size,int y_size);
+
+#define X_SIZE 76
+#define Y_SIZE 95
+
+void get_image(char filename[200],unsigned char in[X_SIZE*Y_SIZE],int x_size,int y_size);
 #endif
 
