299a456a22ce505809c225df5668312276930021
Tony Tan <doublskyp@gmail.com>
2015-09-15 13:38:47 -0500

    add macro

--- put_image.h
+++ put_image.h
@@ -1,5 +1,9 @@
 #ifndef MY_PUTIMAGE_H
 #define MY_PUTIMAGE_H
 #include <stdio.h>
+
+#define X_SIEZ 76
+#define Y_SIEZ 95
+
 void put_image(char filename[100],char *in,int x_size,int y_size);
 #endif
