b0448b9f4221e959b20129db73757b959f7c5eb6
Tony Tan <doublskyp@gmail.com>
2015-09-15 13:45:07 -0500

    convert *in to array

--- susan_edges.h
+++ susan_edges.h
@@ -7,5 +7,5 @@
 #define X_SIZE 76
 #define Y_SIZE 95
 
-void susan_edges(unsigned char *in,int *r,unsigned char mid[76*95],unsigned char bp[516],int x_size,int y_size);
+void susan_edges(unsigned char in[X_SIZE*Y_SIZE],int *r,unsigned char mid[76*95],unsigned char bp[516],int x_size,int y_size);
 #endif
