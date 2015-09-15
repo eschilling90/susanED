4899ff3394109a630cf7f6175db46c7832abbd60
Tony Tan <doublskyp@gmail.com>
2015-09-15 13:28:37 -0500

    add macro

--- susan_edges.h
+++ susan_edges.h
@@ -3,5 +3,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+
+#define X_SIZE 76
+#define Y_SIZE 95
+
 void susan_edges(unsigned char *in,int *r,unsigned char mid[76*95],unsigned char bp[516],int x_size,int y_size);
 #endif
